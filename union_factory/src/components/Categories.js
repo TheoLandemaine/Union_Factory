d01b1fd1b6eadb749f45055c6b48a33222a92071
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-01-17 14:20:31 +0100

    create css file + add picture

--- union_factory/src/components/Categories.js
+++ union_factory/src/components/Categories.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import imgEnvironnement from "../images/fondEnvironnementApp.jpeg";
+import imgHumanitaire from "../images/fondHumanitaireApp.png"
+import imgAnimalier from "../images/fondAnimalApp.png"
+import "../css/Categories.css";
 
 //JS de Rui pour les Cards 
 
@@ -6,9 +10,23 @@ const Categories = () => (
     <>
         <h1>Catégories </h1>
 
+        <div id="CategorieContainer">
+
+            <div id="Environnement" class="image">
+                <img src={imgEnvironnement}></img>
+            </div>
+
+            <div id="Humanitaire" class="image">
+                <img src={imgHumanitaire}></img>
+            </div>
+
+            <div id="Animalier" class="image">
+                <img src={imgAnimalier}></img>
+            </div>
+
+        </div>
     </>
 )
-    
 
 
 export default Categories;
