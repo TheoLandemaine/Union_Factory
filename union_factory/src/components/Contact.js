d7ae6928eb821c0f9e2de58c071799dcf4dd45b1
ilan <ilan.petiot@icloud.com>
2022-01-17 15:31:34 +0100

    ✨ - Start HTML for contact

--- union_factory/src/components/Contact.js
+++ union_factory/src/components/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "../css/contact.css";
 
 const Contact = () => (
     <>
-        <h1>Contact </h1>
+        <div className="contact-container">
+            <h1>Contact </h1>
+            <div className="information-container">
+                <p><span className="titre-contact">Civilité :</span>
+                    <input type="checkbox" id="masculin"/> Masculin
+                    <input type="checkbox" id="feminin"/> Feminin
+                </p>
 
+                <p><span className="titre-contact">Nom :</span> <input type="text" placeholder="Entrer votre nom" required/>   </p>
+                <p><span className="titre-contact">Adresse :</span> <input type="text" placeholder="Entrer votre adresse" required/>    </p>
+                <p><span className="titre-contact">E-mail :</span>  <input type="email" placeholder="Entrer votre E-mail" required/>    </p>
+                <p><span className="titre-contact">Pays :</span>        </p>
+            </div>
+        </div>
     </>
 )
     
