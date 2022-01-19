9cc8bcc70474ad025f926ae8466f8312697dceb8
ilan <ilan.petiot@icloud.com>
2022-01-19 15:23:05 +0100

    📝 - Add useScript

--- union_factory/src/components/Animalier.js
+++ union_factory/src/components/Animalier.js
@@ -1,40 +1,36 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import useScript from '../javaScript/UseScript';
 
 import "../css/cards.css";
 import "../css/Categories.css";
 
 import Cards from "./Cards";
-import React from "react";
 import asso  from './Assos';
 
 
-const Animalier = () => (
-    <div >
-         <div className="nomPage">
-             <h1 id="AnimalierTitrePage">Animalier</h1>
+const Animalier = () => {
+    useScript('./assets/js/animalierScript.js');
+
+    return(
+    <div>
+
+        <div className="nomPage">
+            <h1 id="AnimalierTitrePage">Animalier</h1>
         </div>
 
         <div className="cardCategories">
             <BrowserRouter>
-            <Switch>
-                <Route
-                path="/:filter?"
-                render={(props) => <Cards {...props} cards={asso} />}
-                />
-            </Switch>
+                <Switch>
+                    <Route
+                        path="/:filter?"
+                        render={(props) => <Cards {...props} cards={asso}/>}
+                    />
+                </Switch>
             </BrowserRouter>
         </div>
     </div>
-);
+    )
+}
 
 export default Animalier;
 
-window.addEventListener("DOMContentLoaded", function() {
-    // do stuff
-
-    let card = document.querySelector(".Humanitaire").parentNode.parentNode
-    console.log(card)
-    card.style.display = "none"
-    // console.log(document.querySelector(".Humanitaire").parentNode.parentNode)
-
-}, false);
