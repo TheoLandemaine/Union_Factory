60bb8e23e6a4ac5e1c204b87e9dc22660b65e04a
ilan <ilan.petiot@icloud.com>
2022-01-21 11:42:10 +0100

    ✏️ - Add useScript in Humanitaire

--- union_factory/src/components/Humanitaire.js
+++ union_factory/src/components/Humanitaire.js
@@ -6,28 +6,30 @@ import "../css/Categories.css";
 import Cards from "./Cards";
 import React from "react";
 import asso  from './Assos';
+import useScript from "../javaScript/UseScript";
 
 
 
-const Humanitaire = ({ cards }) => (
-    <>
-        <div className="nomPage">
-            <h1 id="HumanitaireTitrePage">Humanitaire </h1>
-        </div>
-        <div className="cardCategories">
-            <BrowserRouter>
-            <Switch>
-                <Route
-                path="/:filter?"
-                render={(props) => <Cards {...props} cards={asso} />}
-                />
-            </Switch>
-            </BrowserRouter>
-        </div>
+const Humanitaire = () => {
+    useScript('./assets/js/humanitaireScript.js');
+    return (
+        <>
+            <div className="nomPage">
+                <h1 id="HumanitaireTitrePage">Humanitaire </h1>
+            </div>
+            <div className="cardCategories">
+                <BrowserRouter>
+                    <Switch>
+                        <Route
+                            path="/:filter?"
+                            render={(props) => <Cards {...props} cards={asso}/>}
+                        />
+                    </Switch>
+                </BrowserRouter>
+            </div>
 
-    </>
-)
-    
-
+        </>
+    )
+}
 
 export default Humanitaire;
