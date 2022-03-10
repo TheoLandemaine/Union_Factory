c6db8df8a3a349f6a97f8537e703f16ab5afa6e1
ilan <ilan.petiot@icloud.com>
2022-03-10 14:33:09 +0100

    ⚙️- Change Card with database

--- union_factory/src/components/CardsGen.js
+++ union_factory/src/components/CardsGen.js
@@ -2,26 +2,55 @@ import React from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 class CardsGen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+    componentDidMount() {
+        fetch('http://localhost:3002/api/get')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    }
+
   render() {
-    return (
-      <div className="card">
-        <div className="card-image">
-          <img src={this.props.card.img} alt="association" />
-        </div>
-        <div className="card-text">
-          <span className="association">{this.props.card.title}</span>
-          <p className={this.props.card.categorie}>{this.props.card.text}</p>
-          <a className="inc button" href={this.props.card.link} target="_blank" rel="noreferrer">
-            Cliquez pour visiter
-          </a>
-            <i className="heart-filled"><AiFillHeart /></i>
-            <i className="heart-unfilled" hidden><AiOutlineHeart /></i>
-        </div>
-      </div>
-    );
+      var {isLoaded, items} = this.state;
+      if (!isLoaded) {
+          return <div>Loading...</div>;
+      } else {
+
+          return (
+
+              <>
+                  {items.map(item => (
+                      <div className="card" key={item.a_assoID}>
+                          <div className="card-image">
+                              <img src={item.a_image} alt="association"/>
+                          </div>
+                          <div className="card-text">
+                              <span className="association">{item.a_titre}</span>
+                              <p  className={item.a_categorie}>{item.a_description}</p>
+                              <a className="inc button" href={item.a_lien} target="_blank" rel="noreferrer">
+                                  Cliquez pour visiter
+                              </a>
+                              <i className="heart-filled"><AiFillHeart/></i>
+                              <i className="heart-unfilled" hidden><AiOutlineHeart/></i>
+                          </div>
+                      </div>
+                ))}
+            </>
+          );
+
+      }
   }
 }
 
 export default CardsGen;
-
-console.log('je suis ton pere');
