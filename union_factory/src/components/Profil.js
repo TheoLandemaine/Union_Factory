c0588bec4ede5a03e168cb1ce22b3900d516e419
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-03-28 11:56:17 +0200

    change design login

--- union_factory/src/components/Profil.js
+++ union_factory/src/components/Profil.js
@@ -6,39 +6,30 @@ import Footer         from './Footer';
 const Profil = () => (
     <>
         <div className="profilBody">
-            <div className="nomPage">
-                <h1 id="connect">Connectez-vous</h1>     
-            </div>
            
             <section className="container">
-                <div className="cadre">
+                <div className="cadreConnection">
                     <div>
                         <FaUserCircle id="ConnectionLogo"/>
                     </div>
 
                     <div className='mail'>
                   
-                        <div>
-                            <h2 id="TitreAdresseMail">Adresse-mail:</h2>
+                        <div className="div_AdresseMail">
+                            <h2 id="TitreAdresseMail">Adresse-mail :</h2>
                             <input type='text' placeholder="Entrer votre E-mail" id="InputAdresseMail" required/>
                         </div>
 
                         <div className='mdp'>
-                            <h2 id="TitreMotDePasse"> Mot de passe:</h2>
+                            <h2 id="TitreMotDePasse"> Mot de passe :</h2>
                             <input type='text' placeholder="Entrer votre mot de passe" id="InputMotDePasse" required/>
-                            
                         </div>
 
-                        <div className="DivCheckbox">
-                            <input type='checkbox' name="mdp" id="CheckMotDePasse"></input>
-                            <label for="mdp">Mémoriser mot de passe </label> 
-                        </div>
+                        <button className="btn_connection">Se connecter</button>
 
-                        <input type="button" value="Se connecter" id="BoutonConnection"/>
-
-                        <div className='DivIncription'> 
-                            <label for="sign-up">Vous n'avez pas de compte ? </label>
-                            <input id="BoutonIncription" type="button" value="S'inscire ici"/>
+                        <div className="text_inscription">
+                            <span className="sign-up"> Vous n'avez pas de compte ? </span>
+                            <a id="BoutonIncription" href="#"> S'inscire ici </a>
                         </div>
                     </div>
                 </div>
