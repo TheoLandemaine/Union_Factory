2ee35dace92f50170e5064873f6b8d637643c989
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-03-28 12:23:06 +0200

    design login

--- union_factory/src/components/Profil.js
+++ union_factory/src/components/Profil.js
@@ -15,14 +15,34 @@ const Profil = () => (
 
                     <div className='mail'>
                   
-                        <div className="div_AdresseMail">
+                        {/* <div className="div_AdresseMail">
                             <h2 id="TitreAdresseMail">Adresse-mail :</h2>
                             <input type='text' placeholder="Entrer votre E-mail" id="InputAdresseMail" required/>
+                        </div> */}
+                        
+                        <div className="div_AdresseMail">
+                            <div class="text-input-container">
+                                <input class="text-input" type="text" placeholder=" " id="InputAdresseMail" autocomplete="off" required/>
+                                <div class="input-underline"></div>
+                                <div class="label-container">
+                                    <span class="input-label">Adresse-mail :</span>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className='mdp'>
+                        {/* <div className='mdp'>
                             <h2 id="TitreMotDePasse"> Mot de passe :</h2>
                             <input type='text' placeholder="Entrer votre mot de passe" id="InputMotDePasse" required/>
+                        </div> */}
+
+                        <div className="mdp">
+                            <div class="text-input-container">
+                                <input class="text-input" type="text" placeholder=" " id="InputAdresseMail" autocomplete="off" required/>
+                                <div class="input-underline"></div>
+                                <div class="label-container">
+                                    <span class="input-label">Mot de passe :</span>
+                                </div>
+                            </div>
                         </div>
 
                         <button className="btn_connection">Se connecter</button>
