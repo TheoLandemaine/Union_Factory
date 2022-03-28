ed39f90a13034619ae3d9cf2caa4efa3a8c1efe1
ilan <ilan.petiot@icloud.com>
2022-03-28 15:35:12 +0200

    Connect Animalier with bdd

--- union_factory/src/components/Animalier.js
+++ union_factory/src/components/Animalier.js
@@ -1,36 +1,51 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import useScript from '../javaScript/UseScript';
-
+import React from 'react';
+import {useState , useEffect} from 'react';
+import {AiFillHeart, AiOutlineHeart} from "react-icons/ai";
 import "../css/cards.css";
-import "../css/Categories.css";
 
-import Cards from "./Cards";
-import asso  from './Assos';
+function Animalier() {
 
+    const [data, setData] = useState([]);
 
-const Animalier = () => {
-    useScript('./assets/js/animalierScript.js');
+    useEffect(() => {
+        fetch('http://localhost:3002/api/get')
+            .then(res => res.json())
+            .then(json => setData(json))
+    }, []);
 
-    return(
-    <div>
-
-        <div className="nomPage">
-            <h1 id="AnimalierTitrePage">Animalier</h1>
-        </div>
-
-        <div className="cardCategories">
-            <BrowserRouter>
-                <Switch>
-                    <Route
-                        path="/:filter?"
-                        render={(props) => <Cards {...props} cards={asso}/>}
-                    />
-                </Switch>
-            </BrowserRouter>
-        </div>
-    </div>
-    )
+    return (
+        <>
+            <div className="nomPage">
+                <h1 id="AnimalierTitrePage">Animalier </h1>
+            </div>
+            <div className="container">
+                <div className="cards-container">
+                    {data.filter((association)=>{
+                        return association.a_categorie.toString().toLowerCase().includes('Animalier'.toString().toLowerCase());
+                    }).map(association => {
+                        // console.log(association.a_titre);
+                        return (
+                            <div className="card" key={association.a_assoID} id={association.a_assoID}>
+                                <div className="card-image">
+                                    <img src={association.a_image} alt="association"/>
+                                </div>
+                                <div className="card-text">
+                                    <span className="association">{association.a_titre}</span>
+                                    <p className={association.a_categorie}>{association.a_description}</p>
+                                    <a className="inc button" href={association.a_lien} target="_blank" rel="noreferrer">
+                                        Cliquez pour visiter
+                                    </a>
+                                    <i className="heart-filled"><AiFillHeart/></i>
+                                    <i className="heart-unfilled" hidden><AiOutlineHeart/></i>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <p className="no-results" hidden>Aucun résultat</p>
+            </div>
+        </>
+    );
 }
 
 export default Animalier;
-
