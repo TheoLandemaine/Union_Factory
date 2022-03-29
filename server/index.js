e5c60b704e3e57655f902f487e1489ae4087548e
ilan <ilan.petiot@icloud.com>
2022-03-29 15:49:41 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,11 +15,11 @@ const app = express();
 
 const { OAuth2Client } = require('google-auth-library');
 
-app.use(cors());
-
+// app.use(cors());
+//
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
