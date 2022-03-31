63e3c74d58cb5fbb35a7e05fd667d55a5fe40c28
ilan <ilan.petiot@icloud.com>
2022-03-31 20:19:34 +0200

    Display favoris Cards

--- src/components/Favoris.js
+++ src/components/Favoris.js
@@ -1,18 +1,91 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "../css/favoris.css";
 import Footer         from './Footer';
+import Axios from "axios";
+import {AiFillHeart, AiOutlineHeart} from "react-icons/ai";
 
 
+function Favoris (){
 
-const Favoris = () => (
-    <>
-        <div className="nomPage">
-            <h1 id="Favoris">Favoris </h1>
-        </div>
-        <Footer />
+    const [data, setData] = useState([]);
+    Axios.defaults.withCredentials = true;
 
-    </>
-)
+    // GET USER NAME FROM BACKEND
+    const [loginStatus, setLoginStatus] = useState('')
+
+    useEffect(() =>{
+        Axios.get("http://localhost:3003/login").then((response) =>{
+            if (response.data.loggedIn === true) {
+                setLoginStatus(response.data.user[0].username);
+                console.log('je suis connecté')
+
+            }
+        })
+
+            Axios.get('http://localhost:3003/api/get/fav').then((response )=> {
+                    setData(response.data);
+                    console.log('data', data);
+                    console.log('response', response.data);
+            })
+
+    }, [])
+
+    console.log('loginStatus',loginStatus)
+    console.log('data',data);
+
+    try {
+        return(
+            <>
+                <div className="nomPage">
+                    <h1 id="Favoris">Favoris </h1>
+                </div>
+                <h1>Hello {loginStatus}</h1>
+
+                <div className="container">
+                    <div className="cards-container">
+                        {data.map(association => {
+                            // console.log(association.a_titre);
+                            return (
+                                <div label = "card_id" className="card" key={association.a_assoID} id={association.a_assoID}>
+                                    <div className="card-image">
+                                        <img src={association.a_image} alt="association"/>
+                                    </div>
+                                    <div className="card-text">
+                                        <span className="association">{association.a_titre}</span>
+                                        <p className={association.a_categorie}>{association.a_description}</p>
+                                        <a className="inc button" href={association.a_lien} target="_blank" rel="noreferrer">
+                                            Cliquez pour visiter
+                                        </a>
+                                        <i className="heart-filled hidden "><AiFillHeart/></i>
+                                        <i className="heart-unfilled " ><AiOutlineHeart/></i>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <p className="no-results" hidden>Aucun résultat</p>
+                </div>
+
+
+                <Footer />
+
+            </>
+        )
+    }
+    catch (e) {
+        console.log(e);
+        return (
+            <>
+                <div className="nomPage">
+                    <h1 id="Favoris">Favoris </h1>
+                </div>
+                <div className="container">
+                    <h1>Vous devez vous connectez</h1>
+                </div>
+            </>
+        )
+    }
+}
     
 
 
